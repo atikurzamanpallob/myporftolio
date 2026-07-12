f3edf9e0162b67c03cf121e9395599d3b047472b
atikurzamanpallob <atikurzamanpallob@gmail.com>
2026-07-12 11:58:48 +0600

    skills resources added

--- lib/core/app_resources/app_images.dart
+++ lib/core/app_resources/app_images.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const homeImage = 'assets/images/home_image.jpg';
   static const profile = 'assets/images/profile.png';
   static const careerImage = 'assets/images/career_image.jpg';
+  static const skillImage = 'assets/images/skill_image.jpg';
 }
